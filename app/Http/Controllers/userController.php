92f3c328981c38471d97870db183b4a4a33f0b81
Fyn-x <fyn-x@github.com>
2020-01-30 13:01:18 +0700

    Update userController.php

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -4,22 +4,42 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Player;
+use Illuminate\Support\Facades\DB;
 
 class userController extends Controller
 {
     public function login(Request $request)
     {
         $get = $request->all();
+        $user = new Player;
         $cek = DB::select( "SELECT * from user where u_email = '".$get['u_email']."'");
-		$hasil=array();
-		// if($cek){
-		// 	$hasil['success']= true;
-		// 	$hasil['$u_id']= $cek['u_id'];
-		// }else{
-		// 	$hasil['success']=false;
-		// 	$hasil['message']="data tidak ditemukan";
-		// }
-		return $hasil;
-        
+        $hasil=array();
+        // echo "<pre>";var_dump($cek);
+        // die();
+		if($cek){
+			$hasil['success']= true;
+			$hasil['u_id']= $cek[0]->u_id;
+		}else{
+            $user->u_name=$get['u_name'];
+            $user->u_email=$get['u_email'];
+            $user->u_avatar=$get['u_avatar'];
+            $user->u_token_fcm=$get['u_token_fcm'];
+            $user->save();
+            // echo "<pre>";var_dump($user);
+            // die();
+            $hasil['success']=true;
+            $hasil['u_id']= $user->u_id;
+		}
+		return $hasil;   
+    }
+    public function store(Request $request)
+    {
+        $get = $request->all();
+        $get->u_name=$request->u_name;
+        $get->u_email=$request->u_email;
+        $get->u_avatar=$request->u_avatar;
+        $get->u_token_fcm=$request->u_token_fcm;   
+        $get->save();
+        return $get;
     }
 }
